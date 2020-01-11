b2980e0d87fb839bf9baa1172bc3110669e7aecd
OkaGouhei <g.oka1975@gmail.com>
2020-01-11 09:40:35 +0900

    修正

--- Character.java
+++ Character.java
@@ -1,30 +1,8 @@
-import javafx.application.Application;
-import javafx.fxml.*;
-import javafx.scene.*;
-import javafx.stage.Stage;
-import javafx.scene.layout.*;
-import javafx.event.ActionEvent;
 import javafx.scene.image.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.animation.AnimationTimer;
-import javafx.animation.Timeline;
-import javafx.animation.KeyFrame;
-import javafx.util.Duration;
-import javafx.event.EventHandler;
-import javafx.scene.paint.Color;
-import javafx.geometry.Insets;
-import javafx.scene.text.Font;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Character{
 	public Image image;
 	public String name;
 	public int hp;
-
 	public Character(String  name,int hp,String photo){
 		this.hp = hp;
 		this.name = name;
